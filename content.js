f145233cd8a7d21bff143ddd7f87414213636120
TherielGabriel <rosasalgabriel@gmail.com>
2025-07-31 13:42:38 -0700

    detecting SPA transitions

--- content.js
+++ content.js
@@ -1,37 +1,76 @@
 console.log("[WK Helper] Content script loaded.");
 
 const EXTRA_INFO_TAG = "wk-extra-info"
+let running = false
 
-chrome.storage.sync.get("apiKey", ({ apiKey }) => {
-  if (!apiKey) {
-    console.warn("[WK Helper] No API key found in storage.");
-    return;
+let lastUrl = location.href; 
+new MutationObserver(() => {
+  const url = location.href;
+  if (url !== lastUrl) {
+    lastUrl = url;
+    onUrlChange();
   }
-  console.log("[WK Helper] Found API key, fetching subjects...");
+}).observe(document, {subtree: true, childList: true});
 
-  const kanjiElement = document.getElementsByClassName("character-header__characters")[0]
+// Attempt to run extension
+runExtension()
 
-  // Initial UI update
-  const kanji = kanjiElement ? kanjiElement.textContent.trim() : null
-  searchForKanji(apiKey, kanji).then(res => {
-    [date, level] = res
-    displayNewInfo(date, level)
-  })
+function onUrlChange() {
+  const isReviewPage = location.pathname.startsWith("/subjects/review") ||
+                       location.pathname.startsWith("/subjects/extra_study");
+  if (isReviewPage && !running) {
+    console.log("running extension: ", location.href)
+    runExtension()
+  } else {
+    running = false
+  }
+}
 
-  // Subsequent UI updates
-  const observer = new MutationObserver(() => {
+function runExtension() {
+  chrome.storage.sync.get("apiKey", ({ apiKey }) => {
+    if (!apiKey) {
+      console.warn("[WK Helper] No API key found in storage.");
+      return;
+    }
+    console.log("[WK Helper] Found API key...");
+    running = true
+
+    const kanjiElement = document.getElementsByClassName("character-header__characters")[0]
+    if (!kanjiElement) {
+      running = false
+      return
+    }
+
+    // Initial UI update
     const kanji = kanjiElement ? kanjiElement.textContent.trim() : null
-
+    if (!kanji) {
+      running = false
+      return
+    }
     searchForKanji(apiKey, kanji).then(res => {
       [date, level] = res
       displayNewInfo(date, level)
     })
-  });
-  observer.observe(kanjiElement, { childList: true, subtree: true });
-});
+
+    // Subsequent UI updates
+    const reviewObserver = new MutationObserver(() => {
+      const kanji = kanjiElement ? kanjiElement.textContent.trim() : null
+      if (!kanji) {
+        running = false
+        return
+      }
+
+      searchForKanji(apiKey, kanji).then(res => {
+        [date, level] = res
+        displayNewInfo(date, level)
+      })
+    });
+    reviewObserver.observe(kanjiElement, { childList: true, subtree: false });
+  })
+}
 
 async function searchForKanji(apiKey, kanji) {
-    return fetch("https://api.wanikani.com/v2/subjects?slugs=" + kanji, {
+  return fetch("https://api.wanikani.com/v2/subjects?slugs=" + kanji, {
     headers: { Authorization: `Bearer ${apiKey}` }
   })
     .then(res => res.json())
@@ -53,11 +92,10 @@ async function searchForKanji(apiKey, kanji) {
       
       return [formatDate(dateUnlocked), level]
     })
-    .catch(err => {
-        console.error("[WK Helper] Failed to fetch subjects", err)
-        return null
-    }
-    );
+  .catch(err => {
+      console.error("[WK Helper] Failed to fetch subjects", err)
+      return null
+  });
 }
 
 function formatDate(rawDate) {
